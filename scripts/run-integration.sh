31f04b8c32e7d106fdc74c1ef8686a7cc3693ffa
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2025-01-01 12:47:24 +0530

    Update : test.yml, run-integration

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,6 +1,6 @@
 docker compose up -d
 echo "Waiting for DB ready for connection"
-bash ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
+./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
 echo "DB is ready for connection"
 npx prisma migrate dev --name init
 npm run test
